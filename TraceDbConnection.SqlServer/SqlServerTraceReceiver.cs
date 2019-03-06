eacf618578d1d45907dd22f1018bf11f8e1f9217
Dzianis Dziarkach <Dzianis_Dziarkach@epam.com>
2019-03-07 02:45:11 +0300

    added command tsql adapter

---
+++ TraceDbConnection.SqlServer/SqlServerTraceReceiver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using TraceDbConnection.SqlServer.Translation;
+using TraceDbConnection.TraceReporting.Command;
+
+namespace TraceDbConnection.SqlServer
+{
+    public abstract class SqlServerTraceReceiver: ITraceReceiver
+    {
+        public void Save(DbCommand command, ICommandTraceEntry traceEntry)
+        {
+            if(traceEntry is null)
+                throw new ArgumentNullException(nameof(traceEntry));
+
+            if (command is null)
+                throw new ArgumentNullException(nameof(command));
+
+            if (!(command is SqlCommand))
+                throw new InvalidOperationException("Command is no SqlCommand.");
+
+            var tsql = new TSqlCommandToTextTranslator()
+                .Translate((SqlCommand)command);
+            Save(tsql, command, traceEntry);
+        }
+
+        protected abstract void Save(string tsql, DbCommand command, ICommandTraceEntry traceEntry);
+    }
+}
---
+++ TraceDbConnection.SqlServer/Translation/TSqlCommandToTextTranslator.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+
+namespace TraceDbConnection.SqlServer.Translation
+{
+    public class TSqlCommandToTextTranslator
+    {
+        public string Translate(SqlCommand command)
+        {
+            var sb = new StringBuilder();
+
+            var declareBuilder = new TSqlDeclareBuilder();
+            var declareTsql = declareBuilder.Build(command);
+
+            if (declareTsql != string.Empty)
+            {
+                sb.Append(declareTsql)
+                    .AppendLine()
+                    .AppendLine(); // append free line after declarations
+            }
+
+            sb.Append(command.CommandText);
+
+            return sb.ToString();
+        }
+    }
+}
